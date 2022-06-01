118c65157e6c999552b15f05a5759dd812e85cc0
faidatul <faidatul2205@gmail.com>
2022-06-01 14:19:53 +0700

    card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,13 +3,19 @@
         <div class="col-md-12 mb-5">
             <img src="{{ url('images/logo.png') }}" class="rounded mx-auto d-block" width="300" alt="">
         </div>
+        
         @foreach($barangs as $barang)
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg mt-8">
+            <img class = "object-cover h-2 max-w-12" src="{{ url('uploads') }}/{{ $barang->gambar }}" alt="gambar produk">
+            <div class="px-6 py-4">
+                <p class="font-bold text-xl mb-2">{{ $barang->nama_barang }}</p><br>
+            </div>
+                <div class="px-6 pt-4 pb-px">
+                    <span>Harga :</span> Rp. {{ number_format($barang->harga)}} <br>
+                    <span>Stok :</span> {{ $barang->stok }} <br>
+                    <span>Keterangan : </span>{{ $barang->keterangan }}<br> 
+                </div>
+                <a href="#" class="text-[#f59e0b] font-bold"><i class="fa fa-shopping-cart"></i> Pesan</a><br></br>
         </div>
         @endforeach
     </x-slot>
